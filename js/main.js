8dddb743fd2434c1fce012cb232e6524cd016b4d
mong0000 <99624171+mong0000@users.noreply.github.com>
2022-02-21 13:41:25 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var myFullpage = new fullpage('#fullpage', {
-    menu:'#menu',
-    anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6','section7'],
-    sectionsColor: ['#f2f2f2', '#f2f2f2', '#ffe3bc', '#c8d7f0', '#fdfdfd','#fff'],
-    autoScrolling:true, //스크롤 바를 스크롤하면 한 페이지씩 넘어감(true가 기본)
-    slidesNavigation:true, 
-});
