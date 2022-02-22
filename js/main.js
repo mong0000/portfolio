b34137950e128b72bd0d41555b6c5ff602d6fdba
mong0000 <99624171+mong0000@users.noreply.github.com>
2022-02-22 09:42:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ var myFullpage = new fullpage('#fullpage', {
 });
 
 function winOpen() {
-    window.open("https://mong0000.github.io/seewe","popup","width=500, height=850, scrollbars=no, resizable=no")
+    window.open("https://mong0000.github.io/seewe","popup","width=520, height=850, scrollbars=no, resizable=no")
 }
 function winOpen2() {
-    window.open("https://mong0000.github.io/postofficem/","popup","width=520, height=850, scrollbars=no, resizable=no")
+    window.open("https://mong0000.github.io/postofficem/","popup","width=500, height=850, scrollbars=no, resizable=no")
 }
 
 $('.left').mouseover(function(){
@@ -19,4 +19,24 @@ $('.left').mouseover(function(){
 
 $('.right').mouseover(function(){
     $(this).addClass('aniR');
+});
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
+
+// hovering
+const sec = document.querySelector('a');
+sec.addEventListener('mouseover', ()=>{
+    cursor.style.border = '2px solid rgba(0,0,0,.6)';
+    cursor.style.width = '5em';
+    cursor.style.height = '5em';
+})
+
+sec.addEventListener('mouseout', ()=>{
+    cursor.style.border = '20px solid rgba(255,255,255,.6)';
+    cursor.style.width = '0';
+    cursor.style.height = '0';
 });
